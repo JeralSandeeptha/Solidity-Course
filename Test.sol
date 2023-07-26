222f4be715eeac86cb415e9e80b837d2d4a45991
M.N. Jeral Sandeeptha <jeral.sandeeptha1@gmail.com>
2023-07-26 17:19:57 +0530

    About contract class and complier version

---
+++ Test.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+//The SPDX-License-Identifier comment is used to specify the license under which the smart contract is released. 
+//It's a way to communicate the licensing information of the contract and make it easily accessible for developers and users.
+
+//When you include the SPDX-License-Identifier comment in your Solidity smart contract, it serves as a clear indication of the license terms under which the contract's code is published. 
+//This is particularly important for open-source projects as it helps others understand the permissions and restrictions related to the contract's usage, distribution, and modification.
+
+//The license specified is "MIT." The MIT license is a permissive open-source license that allows users to use, modify, and distribute the code while providing only minimal restrictions. 
+//It's one of the most commonly used open-source licenses in the software development community.
+
+//Including the SPDX-License-Identifier comment is considered a best practice, especially for public repositories and contracts, as it helps ensure compliance with licensing requirements and makes it easier for others to use your code responsibly.
+
+contract Test{
+
+//A smart contract in the context of Ethereum and Solidity is a self-executing contract with the terms of the agreement directly written into code. 
+//It resides on the Ethereum blockchain and can perform actions when specific conditions are met.
+
+}
